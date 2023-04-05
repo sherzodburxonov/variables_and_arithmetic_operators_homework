5d323a5c7c1cb1f3dc71c86928fc9ace8a42e1b8
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-05 17:20:02 +0500

    02

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -3,4 +3,5 @@ a=3
 # Create a variable "b"  and assign it the value of the int 4.
 b=4
 # Create a variable called 'answer' and assign it the sum of a and b.
-print('Javob :'+str(a+b))
+answer=a+b
+print(answer)

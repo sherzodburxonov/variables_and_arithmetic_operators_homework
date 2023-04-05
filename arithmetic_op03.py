cf5d3e9ba1c9bc81323c81b337f09e747958bbe9
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-05 17:22:27 +0500

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,4 +3,5 @@ a=6
 # Create a variable "b"  and assign it the value of the int 2.
 b=2
 # Create a variable called 'answer' and assign it the difference of a and b.
-print("Javob : "+str(a-b))
+answer=a-b
+print(answer)

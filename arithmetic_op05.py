d510cfdec09af102cf1953e577fbc7624ef5f4b1
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-05 17:25:17 +0500

    05

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -3,4 +3,5 @@ a=72
 # Create a variable "b"  and assign it the value of the int 9.
 b=9
 # Create a variable called "answer" and assign it the division of a by b.
-print("Javob :"+str(a/b))
+answer=a/b
+print(answer)
